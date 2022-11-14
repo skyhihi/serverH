9b02f9d77859e040f8a315ed21320270944629d3
skyhihi <fahja246@gmail.com>
2022-11-15 01:28:15 +0700

    fah update 15-11 1

--- config/conDB.js
+++ config/conDB.js
@@ -2,11 +2,11 @@ const mariadb = require("mariadb");
 
 async function connectDB(conn_query, values = []) {
   const pool = mariadb.createPool({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "evaluate",
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    port: process.env.PORT_AWS,
     //connectionLimit: process.env.connectionLimit,
   });
   const conn = await pool.getConnection();
